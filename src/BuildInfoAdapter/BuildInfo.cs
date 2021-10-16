59ca9d6bf01f1a41b2c3eb9aa3777e1e68696793
Dave Pusey <dave@puseyuk.co.uk>
2021-10-16 09:11:21 +0100

    BuildInfoAdapter 1.0.21289.0805

---
+++ src/BuildInfoAdapter/BuildInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace BuildInfoAdapter
+{
+    public static class BuildInfo
+    {
+        private static readonly Assembly a = Assembly.GetCallingAssembly();
+        private static readonly AssemblyName an = a.GetName();
+        private static readonly Version v = an.Version;
+
+        public static readonly string Name = an.Name;
+        public static readonly string FullName = an.FullName;
+        public static readonly string Version = v.ToString();
+
+        [Obsolete("BuildDate is deprecated, please use Version instead.")]
+        public static readonly string BuildDate = v.Build + "." + v.Revision;
+
+        // currently unimplemented - provided for compatibility
+        public static readonly string BuildComputer = "";
+        public static readonly string BuildUser = "";
+    }
+}
